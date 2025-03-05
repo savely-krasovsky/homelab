1b96ceee9bef091579b61123235053848c535ce1
Savely Krasovsky <savely@krasovs.ky>
2025-03-05 17:07:10 +0100

    feat: quadlets could now contain everything related: pods, networks, volumes; most containers now run under non-root user

--- fcos.tf
+++ fcos.tf
@@ -1,35 +1,13 @@
 locals {
   butane_config = merge(var.fcos_config, {
-    quadlets = local.quadlets
-    networks = local.networks
+    quadlet_files = local.quadlet_files
   })
 
   # Get a list of all files in the specified directory
   quadlet_paths = fileset(path.module, "quadlets/**")
-  quadlets = {
+  quadlet_files = {
     for path in local.quadlet_paths :
-    replace(basename(path), ".container.tftpl", "") => templatefile(path, var.quadlets_config)
-  }
-
-  # Same with networks
-  network_paths = fileset(path.module, "networks/*")
-  networks = {
-    for path in local.network_paths :
-    replace(basename(path), ".network", "") => file(path)
-  }
-
-  # Bitwarden Secret Manager Secret IDs
-  secrets = {
-    traefik-cf-dns-api-token           = "e9e0f0f0-abc8-4bde-b05f-b292018179bb"
-    oauth2-proxy-cookie-secret         = "289c0832-27c2-463b-97b7-b29200a8cebd"
-    oauth2-proxy-client-secret         = "afdb8ef2-a3d4-4a17-b839-b29200ab6f87"
-    pocket-id-maxmind-license-key      = "08c549a4-bf48-4998-8cb0-b29200ac845d"
-    actual-budget-openid-client-secret = "5754702b-d9d5-4127-b5ab-b29200abdd6a"
-    open-webui-oauth-client-secret     = "b595040b-a23a-44af-8bff-b29200ad6258"
-    hoarder-oauth-client-secret        = "784d379b-bcaf-424f-bc77-b29500ff1be6"
-    hoarder-openai-api-key             = "98f5ccdf-d4b1-4883-b4e3-b295010ba589"
-    meili-master-key                   = "a67874c5-95c2-4f7a-b335-b295010010e0"
-    nextauth-secret                    = "94b4b746-f005-46e0-b60a-b29501010c06"
+    replace(basename(path), ".tftpl", "") => templatefile(path, var.quadlets_config)
   }
 
   init_script_path = "${path.module}/scripts/init_fcos.sh.tftpl"
@@ -54,14 +32,14 @@ resource "proxmox_virtual_environment_vm" "fcos" {
   }
 
   cpu {
-    cores = 4
-    affinity = "26-29"
+    cores = 8
+    affinity = "22-29"
     type = "host"
   }
 
   memory {
-    dedicated = 16384
-    floating  = 16384
+    dedicated = 32768
+    floating  = 32768
   }
 
   efi_disk {
@@ -132,8 +110,8 @@ resource "null_resource" "fcos_provision_secrets" {
     destination = "/tmp/init.sh"
     content = templatefile(local.init_script_path, {
       bws_access_token: var.bws_access_token
-      quadlets: local.quadlets
-      secrets: local.secrets
+      quadlet_files: local.quadlet_files
+      secrets: var.quadlets_secret_config
     })
   }
 
--- variables.tf
+++ variables.tf
@@ -20,6 +20,24 @@ variable "quadlets_config" {
   })
 }
 
+variable "quadlets_secret_config" {
+  description = "Quadlet Ssecret"
+  type = map(string)
+  default = {
+    traefik-cf-dns-api-token           = "e9e0f0f0-abc8-4bde-b05f-b292018179bb"
+    oauth2-proxy-cookie-secret         = "289c0832-27c2-463b-97b7-b29200a8cebd"
+    oauth2-proxy-client-secret         = "afdb8ef2-a3d4-4a17-b839-b29200ab6f87"
+    pocket-id-maxmind-license-key      = "08c549a4-bf48-4998-8cb0-b29200ac845d"
+    actual-budget-openid-client-secret = "5754702b-d9d5-4127-b5ab-b29200abdd6a"
+    open-webui-oauth-client-secret     = "b595040b-a23a-44af-8bff-b29200ad6258"
+    hoarder-oauth-client-secret        = "784d379b-bcaf-424f-bc77-b29500ff1be6"
+    hoarder-openai-api-key             = "98f5ccdf-d4b1-4883-b4e3-b295010ba589"
+    meili-master-key                   = "a67874c5-95c2-4f7a-b335-b295010010e0"
+    nextauth-secret                    = "94b4b746-f005-46e0-b60a-b29501010c06"
+    immich-postgres-password           = "386f1adc-878f-4755-a06b-b29700b15cd0"
+  }
+}
+
 variable "fcos_config" {
   description = "Fedora CoreOS Configuration"
   type = object({

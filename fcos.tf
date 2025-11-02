3afaf845e9787e28642e56462b619ee6ed5b792a
Savely Krasovsky <savely@krasovs.ky>
2025-11-02 15:18:30 +0100

    feat: update livekit and victoria logs

--- fcos.tf
+++ fcos.tf
@@ -147,8 +147,8 @@ resource "proxmox_virtual_environment_vm" "fcos" {
   }
 
   memory {
-    dedicated = 49152
-    floating  = 49152
+    dedicated = 32768
+    floating  = 32768
   }
 
   efi_disk {
@@ -178,7 +178,7 @@ resource "proxmox_virtual_environment_vm" "fcos" {
     type = "l26"
   }
 
-  # Intel A380 video
+  # Intel Arc Pro B50 video
   hostpci {
     device = "hostpci0"
     id     = "0000:03:00.0"
@@ -186,7 +186,7 @@ resource "proxmox_virtual_environment_vm" "fcos" {
     rombar = true
   }
 
-  # Intel A380 audio
+  # Intel Arc Pro B50 audio
   hostpci {
     device = "hostpci1"
     id     = "0000:04:00.0"
